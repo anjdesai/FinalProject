d82488726de29ef2995a9e1ad43ab180185890f8
anjdesai <76200097+anjdesai@users.noreply.github.com>
2021-01-24 13:55:04 -0800

    Day 29-30: Work Session 4 (6-7:30)
    During this work session I got stuck on #5 of the Project Euler problems. I decide to take the 75 minutes to figure out exactly what the code needs to do. I know that the code needs to loop over all values, starting at 2. If the number is divisible, it should go to the next number until it is no longer divisible. Then it should print the smallest divisible number.

--- lab4.py
+++ lab4.py
@@ -60,4 +60,12 @@ for x in range (100,999):
         if str(palindrome) == str(palindrome)[::-1]:
             if palindrome > Largest3Palindrome:
                 Largest3Palindrome = palindrome
-print(Largest3Palindrome)  
+print(Largest3Palindrome)
+
+
+'''
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+'''
+
